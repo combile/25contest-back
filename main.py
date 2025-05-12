4d90f7c417ebf70736a7cbb22bf91a289c430f2c
Minter0817 <adf5858@naver.com>
2025-05-12 16:18:17 +0900

    Docs: 메인 함수 주석처리

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from src.service.CreateJson import createJson
+from src.service.create_json import *
 
-
-
-if "__name__" == "__main__":
-    createJson(file="엑셀 파일 입력", save_dir="저장 경로 입력")
+if __name__ == "__main__":
+    # MARK: - 뉴스 데이터 json 생성
+    # create_json(file="엑셀 파일 입력", save_dir="저장 경로 입력")
+    
+    #MARK: - 뉴스 데이터 도메인 삽입
+    # insert_json(Path("/Users/minji/Desktop/Test/NewsSummarization/data/output"))
